9dc4b409c2adb6450d92269b6801cb1a3fa42de8
TJBaumgartner <tnorth6@gmail.com>
2023-04-25 12:01:00 -0700

    playRound function and restart game

--- javascript.js
+++ javascript.js
@@ -1,9 +1,8 @@
-const gameBoard = (function () {
+const createGameBoard = (function () {
 
     let gameGrid = document.getElementById('gameGrid');
-    let board = ['1', '1', '1', '4', '5', '6', '7', '8', '9' ];
+    let board = ['', '', '', '', '', '', '', '', '' ];
     const cells = Array.from(document.querySelectorAll('.gridCell'));
-    let winner = null;
 
     const refreshBoard = function() {
         board.forEach(function (mark, idx) {
@@ -12,18 +11,13 @@ const gameBoard = (function () {
     };
 
 
-    function boardObject() {
-        let boardDisplay = board;
-        console.log(boardDisplay)
-        return boardDisplay;
-    }
-
     const winCondition = function() {
+        let winner = null
         for(let i = 0; i < 9; i++){ 
                 if(board.includes('') != true){
                     winner = 'Tie'
                 }
-                if(board[0] === board[1] && board[1] === board[2] && board[i][0] != ''){
+                if(board[0] === board[1] && board[1] === board[2] && board[0] != ''){
                     winner = 'currentPlayer';
                 }
                 if(board[3] === board[4] && board[4] === board[5] && board[3] != ''){
@@ -50,32 +44,113 @@ const gameBoard = (function () {
         }
         return winner;
     }
-
-
-
-
-    return {boardObject, refreshBoard, gameGrid, winCondition};
+    return {board, refreshBoard, gameGrid, winCondition, cells};
 })();
-gameBoard.boardObject()
-gameBoard.refreshBoard()
-gameBoard.winCondition()
+
 const displayController = (function (playerOne = "Player One", playerTwo= "Player Two") {
 
+    const gameBoard = createGameBoard;
     const players = [
         { 
             name: playerOne,
-            markerOne: 'X'
+            marker: 'X'
         },
         { 
             name: playerTwo,
-            markerTwo: 'O'
+            marker: 'O'
         }
     ]
+
+    const playTurn = (board, cell) => {
+        const idx = board.cells.findIndex(position => position === cell);
+        if (createGameBoard.board[idx] === '') {
+          createGameBoard.refreshBoard();
+          return idx;
+        }
+        return null;
+      };
+
     let activePlayer = players[0];
 
 
+    const cell = Array.from(document.querySelectorAll('.gridCell'));
+    const winnerDisplay = document.getElementById('displayWinner');
+
+    const startRound = function() {
+        cell.forEach((cell) => {
+            cell.classList.add('active');
+            cell.addEventListener('click', (event) => {
+                event.preventDefault();
+                const play = playTurn(createGameBoard, event.target);
+                if(cell.classList.contains('active') != true){
+                    return;
+                }
+                if(activePlayer == players[0]){
+                    console.log(gameBoard.board)
+                    cell.classList.remove('active');
+                    gameBoard.board[play] = players[0].marker;
+                    gameBoard.refreshBoard();
+                    if(gameBoard.winCondition() == 'currentPlayer'){
+                        winnerDisplay.textContent = activePlayer.name + ' Wins!';
+                        resetActive();
+                        return;
+                    }
+                    if(gameBoard.winCondition() == 'Tie'){
+                        winnerDisplay.textContent = 'Tie Game!';
+                        resetActive();
+                        return;
+                    }
+                    switchPlayerTurn();
+                    return;
+                }
+                if(activePlayer == players[1]){
+                    cell.classList.remove('active');
+                    gameBoard.board[play] = players[1].marker;
+                    gameBoard.refreshBoard();
+                    if(gameBoard.winCondition() == 'currentPlayer'){
+                        winnerDisplay.textContent = activePlayer.name + ' Wins!';
+                        resetActive();
+                        return;
+                    }
+                    if(gameBoard.winCondition() == 'Tie'){
+                        winnerDisplay.textContent = 'Tie Game!';
+                        resetActive();
+                        return;
+                    }
+                    switchPlayerTurn();
+                    return;
+                }
+            });
+        });
+    }
+    const restartGame = function() {
+        gameBoard.board.forEach((element, index) => {
+            gameBoard.board[index] = '';
+        });
+        activePlayer = 'none';
+        gameBoard.refreshBoard(); 
+        switchPlayerTurn();
+        startRound();
+
+
+    }
+
+    const resetActive = function() {
+        cell.forEach((cell) => {
+            cell.classList.remove('active');
+        });
+    }
+
+    const playRound = function () {
+        startRound();
+    }
+
     
     const switchPlayerTurn = function() {
+        if(activePlayer === 'none'){
+            activePlayer = players[0];
+            return;
+        }
         if(activePlayer === players[0]){
             activePlayer = players[1]
             return;
@@ -85,4 +160,9 @@ const displayController = (function (playerOne = "Player One", playerTwo= "Playe
             return;
         }
     }
+    return {playRound, restartGame}
 })();
+const startButton = document.getElementById('startButton');
+startButton.addEventListener('click', displayController.playRound);
+const restartButton = document.getElementById('restartButton');
+restartButton.addEventListener('click', displayController.restartGame);
